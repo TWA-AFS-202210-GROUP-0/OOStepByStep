5c8239096e85ed5e0a9b3159474b8b6baa207b9a
Jie Li <JLi147@slb.com>
2022-10-27 17:52:23 +0800

    feat: add a person class with name and age and can return a greeting

---
+++ OOStepByStepTest/PersonTest.cs
@@ -0,0 +1,24 @@
+﻿using OOStepByStep;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OOStepByStepTest
+{
+    public class PersonTest
+    {
+        [Fact]
+        public void Shoule_return_greeting_when_introduce_self_given_person_have_name_age()
+        {
+            // given
+            Person person = new Person("Tom", 21);
+            // when
+            string greeting = person.Greeting();
+            // then
+            Assert.Equal("My name is Tom. I am 21 years old", greeting);
+        }
+    }
+}
