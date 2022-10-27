2f69db3773f2145c02fb943422174a703618c334
Xiaotian WU <XWu32@slb.com>
2022-10-27 17:46:06 +0800

    feat: create person class that can introduce itself.

--- OOStepByStepTest/UnitTest1.cs
+++ OOStepByStepTest/UnitTest1.cs
@@ -6,10 +6,14 @@ namespace OOStepByStepTest
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Should_person_can_introduce_himself()
         {
-            var class1 = new Class1();
-            Assert.NotNull(class1);
+            // given
+            var person = new Person("Tom", 21);
+            // when
+            var msg = person.Introduce();
+            // then
+            Assert.Equal(msg, "My name is Tom. I am 21 years old");
         }
     }
 }
