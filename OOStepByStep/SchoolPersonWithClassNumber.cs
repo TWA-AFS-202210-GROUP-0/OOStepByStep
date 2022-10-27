29757d4e13bdca63d917578ae645e85853938de7
Jie Li <JLi147@slb.com>
2022-10-27 18:50:16 +0800

    feat: [AC3] add SchoolPersonWithClassNumber and test with student

---
+++ OOStepByStep/SchoolPersonWithClassNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOStepByStep
+{
+    public class SchoolPersonWithClassNumber : ISchoolPersonWithClassNumber
+    {
+        public SchoolPersonWithClassNumber(string name, int age, string job, int classNumber)
+        {
+            Name = name;
+            Age = age;
+            Job = job;
+            ClassNumber = classNumber;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Job { get; set; }
+        public int ClassNumber { get; set; }
+
+        public string Introduce => $"My name is {Name}. I am {Age} years old. I am a {Job}";
+    }
+}
---
+++ OOStepByStepTest/StudentWithClassTest.cs
@@ -0,0 +1,24 @@
+﻿using OOStepByStep;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OOStepByStepTest
+{
+    public class SchoolPersonWithClassTest
+    {
+        [Fact]
+        public void Should_return_greeting_with_job_class_student_when_introduce_given_student_with_name_age_job_class()
+        {
+            // given
+            SchoolPersonWithClassNumber student = new SchoolPersonWithClassNumber("Tom", 18, "student", 3);
+            // when
+            string introduce = student.Introduce;
+            // then
+            Assert.Equal("My name is Tom. I am 18 years old. I am a student", introduce);
+        }
+    }
+}
